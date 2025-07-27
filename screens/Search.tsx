9669dacf71c09a2f29d6d36f26952a5735dbe122
Vermin84 <verminator84@gmail.com>
2025-07-27 11:53:05 +0300

    in work))

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,23 +1,40 @@
-import {  useState } from "react";
+import {  useCallback, useState } from "react";
 import { Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import debounce from 'lodash.debounce';
 
-import {searchMoviesByQuery} from "../service/api";
 import { useQuerySearch } from "../useHooks/useQuerySearch";
+import { Movie } from "../types/types";
 
 export default function Search(){
     const [query, setQuery]= useState('')
+    const [debouncedQuery, setDebouncedQuery] = useState('');
+    const [searchResult, setSearchResult] = useState<Movie[]>([])
 
-    function queryHandler(e: string){
-        setQuery(e )
-        
-    }
-   const { data: movies, isLoading, isError } = useQuerySearch(query);
+    const debouncedSetQuery = useCallback(
+    debounce((text: string) => {
+      setDebouncedQuery(text);
+    }, 500), // 500 мс — настраиваемо
+    []
+  );
+    function onChangeText(text: string) {
+    setQuery(text);
+    debouncedSetQuery(text);
+  }
+   const { data: movies, isLoading, isError } = useQuerySearch(debouncedQuery);
     
+    function onSubmitHandler(){
+        if(query&& movies){
+            setSearchResult(movies)
+            setQuery('')
+
+        }
+    }
+
     console.log(movies?.length)
     return <SafeAreaView>
         <View>
-            <TextInput value={query} onChangeText={queryHandler} placeholder="Search movie..."/>
+            <TextInput value={query} blurOnSubmit={true} onSubmitEditing={()=>onSubmitHandler()} onChangeText={onChangeText} placeholder="Search movie..."/>
         </View>
         <Text>Search</Text>
     </SafeAreaView>
